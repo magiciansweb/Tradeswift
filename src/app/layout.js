b342fab9fd975a57184d58a6ee440f36c59b8a0a
khaled0915 <khaledsaifullah50956@gmail.com>
2024-01-19 18:55:04 +0600

    navbar,footer and banner created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import Navbar from '@/components/shared/Navbar';
+import Footer from '@/components/shared/Footer';
+
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -16,7 +19,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme={"light"}>
-      <body className={roboto.className}>{children}</body>
+
+      <body className= {`${roboto.className} max-w-screen-xl mx-auto`}
+      
+      >
+        <Navbar></Navbar>
+        {children}
+        <Footer></Footer>
+        </body>
+
+        
+
+
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,62 @@
 
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
-
-
-const Home = () => {
+const HomePage = () => {
   return (
+
+
+ <div className=''>
+
+     <div
+      style={{
+        backgroundImage: 'url(https://i.ibb.co/z6mB4B4/stephen-dawson-qwt-Ce-J5c-LYs-unsplash.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height :'100vh',
+
+      }} 
+      
+      className="flex justify-center items-center h-[100vh] mx-auto  ">
+        <div className="absolute inset-0 bg-black opacity-70"></div>
+
+
+      
+
+        <div className="hero min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+
+   <Image src="https://i.ibb.co/XkJ1S0Q/75ez.gif" alt="banner" width={300} height={300}/>
+
     <div>
-      <h1>Home Page</h1>
+      <h1 className="text-5xl font-bold text-white">Empower Your Business Journey with TradeSwift</h1>
+
+      <p className="py-6 text-lg font-medium text-white">Welcome to TradeSwift, where businesses converge for seamless trade. Our platform is designed to simplify and enhance your trading experience, connecting you with global markets and fostering growth</p>
+     <Link href="signUp">  <button className="btn btn-primary">Get Started</button> </Link>
     </div>
+  </div>
+</div>
+
+
+          
+          
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+    </div>
+ </div>
   );
 };
 
