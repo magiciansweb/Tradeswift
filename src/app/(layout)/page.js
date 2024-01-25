d985eb5cf8892d2f3f3609f355ea5aa3baa5f60a
khaled0915 <khaledsaifullah50956@gmail.com>
2024-01-26 01:16:13 +0600

    add foundation section with two video player using react-modal and react-player

--- src/app/(layout)/page.js
+++ src/app/(layout)/page.js
@@ -1,3 +1,4 @@
+import Foundation from "@/components/Foundation/Foundation";
 import Banner from "@/components/Home/Banner/Banner";
 import Newslatter from "@/components/Home/Newslatter/Newslatter";
 import React from "react";
@@ -5,6 +6,9 @@ import React from "react";
 const HomePage = () => {
   return <div>
     <Banner/>
+
+    <Foundation></Foundation>
+    
     <Newslatter/>
   </div>;
 };
