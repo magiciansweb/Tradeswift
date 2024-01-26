9b4c98858ba2ed3cd74fe6203ef7eceb6231b2f5
shima <shima421914@gmail.com>
2024-01-26 11:44:44 +0600

    added-three-four-features-for-landing-page

--- src/app/(layout)/page.js
+++ src/app/(layout)/page.js
@@ -1,11 +1,20 @@
+import Bannerbottom from "@/components/Bannerbottom";
+import Footertop from "@/components/Footertop";
+import Getstarted from "@/components/Getstarted";
 import Banner from "@/components/Home/Banner/Banner";
-import Newslatter from "@/components/Home/Newslatter/Newslatter";
+import Whyinvest from "@/components/Whyinvest";
+import Clients from "@/components/clients";
 import React from "react";
+
 
 const HomePage = () => {
   return <div>
     <Banner/>
-    <Newslatter/>
+    <Bannerbottom></Bannerbottom>
+    <Whyinvest></Whyinvest>
+   <Clients></Clients>
+   <Getstarted></Getstarted>
+    <Footertop></Footertop>
   </div>;
 };
 
