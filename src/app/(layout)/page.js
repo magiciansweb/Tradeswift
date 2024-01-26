867ac514a01eae4f3d472f1e71d42fb6c60f9d05
magiciansweb <156706494+magiciansweb@users.noreply.github.com>
2024-01-26 20:48:59 +0600

    Update page.js

--- src/app/(layout)/page.js
+++ src/app/(layout)/page.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 import Foundation from "@/components/Foundation/Foundation";
-=======
 import Bannerbottom from "@/components/Bannerbottom";
 import Easytouse from "@/components/Easytouse/page";
 import Footertop from "@/components/Footertop";
 import Getstarted from "@/components/Getstarted";
->>>>>>> e6d01cd86f46fe5534d5b806222b5614cf513603
 import Banner from "@/components/Home/Banner/Banner";
 import Whyinvest from "@/components/Whyinvest";
 import Clients from "@/components/clients";
@@ -15,19 +12,16 @@ import React from "react";
 const HomePage = () => {
   return <div>
     <Banner/>
-<<<<<<< HEAD
 
     <Foundation></Foundation>
     
     <Newslatter/>
-=======
     <Bannerbottom></Bannerbottom>
     <Whyinvest></Whyinvest>
    <Clients></Clients>
    <Getstarted></Getstarted>
    <Easytouse></Easytouse>
     <Footertop></Footertop>
->>>>>>> e6d01cd86f46fe5534d5b806222b5614cf513603
   </div>;
 };
 
