3b94e95ea6725e8c95d15390abf3ae440404eca6
shima <shima421914@gmail.com>
2024-02-02 16:10:35 +0600

    data

--- src/app/(userdashboard)/layout.js
+++ src/app/(userdashboard)/layout.js
@@ -4,8 +4,8 @@
 const layout = ({children}) => {
     return (
         <div className="flex flex-col lg:flex-row container mx-auto">
-            <div><div className="navbar bg-base-100">
-  <div className="navbar-start">
+            <div><div className=" bg-base-100">
+  <div className="">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -24,7 +24,7 @@ const layout = ({children}) => {
     </div>
     <a className="btn btn-ghost text-xl">daisyUI</a>
   </div>
-  <div className="navbar-center hidden lg:flex">
+  <div className=" hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
       <li><a>Item 1</a></li>
       <li>
@@ -39,7 +39,7 @@ const layout = ({children}) => {
       <li><a>Item 3</a></li>
     </ul>
   </div>
-  <div className="navbar-end">
+  <div className="">
     <a className="btn">Button</a>
   </div>
 </div></div>
