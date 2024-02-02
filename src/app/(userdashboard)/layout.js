58f478a364438d5647e342a3163b9f5b19850c2f
shima <shima421914@gmail.com>
2024-02-02 15:56:34 +0600

    data

--- src/app/(userdashboard)/layout.js
+++ src/app/(userdashboard)/layout.js
@@ -1,13 +1,11 @@
-// import Sidebar from "@/components/Sidebar/Sidebar";
 
-import SideBar from "@/components/SideBar";
 
 
 const layout = ({children}) => {
     return (
         <div className="flex flex-col lg:flex-row container mx-auto">
-            <div><SideBar></SideBar></div>
-            {/* <div><Sidebar/></div> */}
+            
+         <h2>userdashboard</h2>
             <div className="px-3 py-3">{children}</div>
         </div>
     );
