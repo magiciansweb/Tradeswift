e641af34459a3eb9f4f38bfad2373d9e8b82503d
shima <shima421914@gmail.com>
2024-02-02 13:31:56 +0600

    data

--- src/app/deposite/page.js
+++ src/app/deposite/page.js
@@ -28,9 +28,9 @@ const Transection = () => {
       
     })
       .then(result=>{
-        console.log(result.url)
+        console.log(result)
 
-        window.location.replace(result.url)
+        // window.location.replace(result.url)
       })
       .catch(error => console.error('Fetch error:', error))
       }
