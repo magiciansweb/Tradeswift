5b417305e6493480783fa9552017373863db5682
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2024-02-16 16:56:56 +0600

    trying to deploy 3

--- src/app/(userdashboard)/userdashboard/tournaments/[tournamentsId]/page.js
+++ src/app/(userdashboard)/userdashboard/tournaments/[tournamentsId]/page.js
@@ -1,12 +1,6 @@
 
-
-<<<<<<< HEAD
 const DetailsPage = ({params}) => {
     console.log(params);
-=======
-const DetailsPage = () => {
-    // console.log(params);
->>>>>>> master
     return (
         <div>
             <h2> Tournament details page </h2>
