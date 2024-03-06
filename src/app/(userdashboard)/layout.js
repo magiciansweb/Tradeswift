2c4c396893a849c34caa80a85e913a44a64e8163
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2024-03-07 00:01:51 +0600

    dashboard responsive by imtiaj

---
+++ src/app/(userdashboard)/layout.js
@@ -0,0 +1,19 @@
+"use client";
+import UseAdmin from "@/components/Hooks/UseAdmin";;
+import SideBar from "@/components/SideBar";
+
+const layout = ({ children }) => {
+  const [userInfo] = UseAdmin();
+  return (
+    <div className="flex flex-col lg:flex-row container">
+    <div className="lg:h-screen lg:w-64 z-50">
+      <SideBar userInfo={userInfo} />
+    </div>
+    <div className="px-4 py-3  lg:w-[calc(100%-256px)]">
+      {children}
+    </div>
+  </div>
+  );
+};
+
+export default layout;
